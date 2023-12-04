5a01eafb4a33cf62b8c3efc9e67ee72fa5638563
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2023-12-04 18:45:57 -0500

    Add more test cases and finish Day 2 part 1

--- Day2/main.go
+++ Day2/main.go
@@ -1,9 +1,94 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	var sumOfValidGameNumbers int64
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		gameNumber := determineIfValidGame(scanner.Text())
+		sumOfValidGameNumbers += gameNumber
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Sum of valid game nubers: %d\n", sumOfValidGameNumbers)
 }
 
-func determineIfValidGame(line string) int {
-	return -1
+func determineIfValidGame(line string) int64 {
+	lineSplit := strings.Split(line, ":")
+
+	re := regexp.MustCompile(`(\d+)$`)
+
+	matches := re.FindAll([]byte(lineSplit[0]), 1)
+
+	gameNumber, err := strconv.ParseInt(string(matches[0]), 0, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	games := strings.Split(lineSplit[1], ";")
+
+	for _, game := range games {
+
+		var numberOfRed int64
+		var numberOfGreen int64
+		var numberOfBlue int64
+
+		redRegex := regexp.MustCompile(`(\d+) red`)
+
+		redMatches := redRegex.FindStringSubmatch(game)
+
+		if len(redMatches) == 2 {
+			numberOfRed, err = strconv.ParseInt(string(redMatches[1]), 0, 64)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		greenRegex := regexp.MustCompile(`(\d+) green`)
+
+		greenMatches := greenRegex.FindStringSubmatch(game)
+
+		if len(greenMatches) == 2 {
+			numberOfGreen, err = strconv.ParseInt(string(greenMatches[1]), 0, 64)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		blueRegex := regexp.MustCompile(`(\d+) blue`)
+
+		blueMatches := blueRegex.FindStringSubmatch(game)
+
+		if len(blueMatches) == 2 {
+			numberOfBlue, err = strconv.ParseInt(string(blueMatches[1]), 0, 64)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		if numberOfRed > 12 || numberOfGreen > 13 || numberOfBlue > 14 {
+			return 0
+		}
+	}
+
+	return gameNumber
 }
--- Day2/main_test.go
+++ Day2/main_test.go
@@ -9,23 +9,31 @@ import (
 func TestDetermineIfValidGame_ValidGames(t *testing.T) {
 	input := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
 	gameNumber := determineIfValidGame(input)
-	assert.Equal(t, 1, gameNumber)
+	assert.Equal(t, int64(1), gameNumber)
 
 	input = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
 	gameNumber = determineIfValidGame(input)
-	assert.Equal(t, 2, gameNumber)
+	assert.Equal(t, int64(2), gameNumber)
 
 	input = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 	gameNumber = determineIfValidGame(input)
-	assert.Equal(t, 5, gameNumber)
+	assert.Equal(t, int64(5), gameNumber)
+
+	input = "Game 3455: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+	gameNumber = determineIfValidGame(input)
+	assert.Equal(t, int64(3455), gameNumber)
 }
 
 func TestDetermineIfValidGame_InvalidGames(t *testing.T) {
 	input := "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
 	gameNumber := determineIfValidGame(input)
-	assert.Equal(t, 0, gameNumber)
+	assert.Equal(t, int64(0), gameNumber)
 
 	input = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
 	gameNumber = determineIfValidGame(input)
-	assert.Equal(t, 0, gameNumber)
+	assert.Equal(t, int64(0), gameNumber)
+
+	input = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 1235235 blue, 1 red"
+	gameNumber = determineIfValidGame(input)
+	assert.Equal(t, int64(0), gameNumber)
 }
