aa67e85f073f8bb80c3f3dd35de9e40de4283a77
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2023-12-04 21:04:15 -0500

    Add day 2 part 2 tests and refactor

--- Day2/main.go
+++ Day2/main.go
@@ -47,48 +47,58 @@ func determineIfValidGame(line string) int64 {
 	games := strings.Split(lineSplit[1], ";")
 
 	for _, game := range games {
-
-		var numberOfRed int64
-		var numberOfGreen int64
-		var numberOfBlue int64
-
-		redRegex := regexp.MustCompile(`(\d+) red`)
-
-		redMatches := redRegex.FindStringSubmatch(game)
-
-		if len(redMatches) == 2 {
-			numberOfRed, err = strconv.ParseInt(string(redMatches[1]), 0, 64)
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		greenRegex := regexp.MustCompile(`(\d+) green`)
-
-		greenMatches := greenRegex.FindStringSubmatch(game)
-
-		if len(greenMatches) == 2 {
-			numberOfGreen, err = strconv.ParseInt(string(greenMatches[1]), 0, 64)
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		blueRegex := regexp.MustCompile(`(\d+) blue`)
-
-		blueMatches := blueRegex.FindStringSubmatch(game)
-
-		if len(blueMatches) == 2 {
-			numberOfBlue, err = strconv.ParseInt(string(blueMatches[1]), 0, 64)
-			if err != nil {
-				panic(err)
-			}
-		}
-
+		numberOfRed, numberOfGreen, numberOfBlue := getNumberOfRGBCubes(game)
 		if numberOfRed > 12 || numberOfGreen > 13 || numberOfBlue > 14 {
 			return 0
 		}
 	}
 
 	return gameNumber
+}
+
+func calculateGamePower(game string) int64 {
+
+	return int64(0)
+}
+
+func getNumberOfRGBCubes(game string) (int64, int64, int64) {
+	var numberOfRed int64
+	var numberOfGreen int64
+	var numberOfBlue int64
+	var err error
+
+	redRegex := regexp.MustCompile(`(\d+) red`)
+
+	redMatches := redRegex.FindStringSubmatch(game)
+
+	if len(redMatches) == 2 {
+		numberOfRed, err = strconv.ParseInt(string(redMatches[1]), 0, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	greenRegex := regexp.MustCompile(`(\d+) green`)
+
+	greenMatches := greenRegex.FindStringSubmatch(game)
+
+	if len(greenMatches) == 2 {
+		numberOfGreen, err = strconv.ParseInt(string(greenMatches[1]), 0, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	blueRegex := regexp.MustCompile(`(\d+) blue`)
+
+	blueMatches := blueRegex.FindStringSubmatch(game)
+
+	if len(blueMatches) == 2 {
+		numberOfBlue, err = strconv.ParseInt(string(blueMatches[1]), 0, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return numberOfRed, numberOfGreen, numberOfBlue
 }
--- Day2/main_test.go
+++ Day2/main_test.go
@@ -37,3 +37,25 @@ func TestDetermineIfValidGame_InvalidGames(t *testing.T) {
 	gameNumber = determineIfValidGame(input)
 	assert.Equal(t, int64(0), gameNumber)
 }
+
+func TestCalculateGamePower_ReturnsPowerCorrectly(t *testing.T) {
+	input := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+	gameNumber := calculateGamePower(input)
+	assert.Equal(t, int64(48), gameNumber)
+
+	input = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+	gameNumber = calculateGamePower(input)
+	assert.Equal(t, int64(12), gameNumber)
+
+	input = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+	gameNumber = calculateGamePower(input)
+	assert.Equal(t, int64(1560), gameNumber)
+
+	input = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+	gameNumber = calculateGamePower(input)
+	assert.Equal(t, int64(630), gameNumber)
+
+	input = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+	gameNumber = calculateGamePower(input)
+	assert.Equal(t, int64(36), gameNumber)
+}
