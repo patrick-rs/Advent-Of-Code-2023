4ee0fb4bf56517340efef025bd0e00c12c32d834
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2023-12-07 20:39:15 -0500

    Add more tests and fixing checkForSymbol

--- Day3/main.go
+++ Day3/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"unicode"
 )
 
@@ -10,22 +9,65 @@ func main() {
 }
 
 func checkIfSymbol(i byte) bool {
-	fmt.Println(i)
 	if unicode.IsDigit(rune(i)) {
 		return false
 	}
 	if unicode.IsLetter(rune(i)) {
 		return false
 	}
+	if i == '.' {
+		return false
+	}
 	return true
 }
 
-func checkForSymbol(x1, y1, length int64, rows []string) bool {
+func checkForSymbol(yPos, xPos, length int64, rows []string) bool {
+
+	checkRightSide := true
+	checkLeftSide := true
+
+	if xPos == -1 {
+		checkLeftSide = false
+		xPos += 1
+		length -= 1
+	}
+
+	if int(xPos+length) == len(rows[0]) {
+		checkRightSide = false
+		length -= 1
+	}
 
 	for x := int64(0); x < length; x++ {
-		if checkIfSymbol(rows[y1][x]) {
+		if checkIfSymbol(rows[yPos][xPos+x]) {
 			return true
 		}
 	}
+
+	if len(rows) == int(yPos+1) {
+		return false
+	}
+
+	if checkRightSide {
+		if checkIfSymbol(rows[yPos+1][xPos]) {
+			return true
+		}
+	}
+
+	if checkLeftSide {
+		if checkIfSymbol(rows[yPos+1][xPos+length]) {
+			return true
+		}
+	}
+
+	if len(rows) == int(yPos+2) {
+		return false
+	}
+
+	for x := int64(0); x < length; x++ {
+		if checkIfSymbol(rows[yPos+2][xPos+x]) {
+			return true
+		}
+	}
+
 	return false
 }
--- Day3/main_test.go
+++ Day3/main_test.go
@@ -10,10 +10,10 @@ import (
 func TestCheckForSymbol_ReturnsTrue(t *testing.T) {
 	input := `..........
 ..........
-.....+....
+......+...
 .........`
 
-	found := checkForSymbol(1, 4, 3, strings.Split(input, "\n"))
+	found := checkForSymbol(2, 4, 3, strings.Split(input, "\n"))
 	assert.True(t, found)
 }
 
@@ -25,6 +25,61 @@ func TestCheckForSymbol_ReturnsFalse(t *testing.T) {
 
 	found := checkForSymbol(1, 4, 2, strings.Split(input, "\n"))
 
+	assert.False(t, found)
+}
+
+func TestCheckForSymbol_ReturnsTrueWhenAtBottom(t *testing.T) {
+	input := `..........
+..........
+..........
+...._....`
+
+	found := checkForSymbol(2, 4, 2, strings.Split(input, "\n"))
+
+	assert.True(t, found)
+}
+
+func TestCheckForSymbol_ReturnsFalseWhenAtBottom(t *testing.T) {
+	input := `..........
+..........
+..........
+.........`
+
+	found := checkForSymbol(3, 4, 2, strings.Split(input, "\n"))
+
+	assert.False(t, found)
+}
+
+func TestCheckForSymbol_ReturnsFalseWhenAtSecondToLastRow(t *testing.T) {
+	input := `..........
+..........
+..........
+.........`
+
+	found := checkForSymbol(3, 3, 2, strings.Split(input, "\n"))
+
+	assert.False(t, found)
+}
+
+func TestCheckForSymbol_ReturnsTrueWhenStartingOnLeftEdge(t *testing.T) {
+	input := `..........
+..........
+&.........
+.........`
+
+	found := checkForSymbol(1, 0, 2, strings.Split(input, "\n"))
+
+	assert.True(t, found)
+}
+
+func TestCheckForSymbol_ReturnsTrueWhenStartingOnRightEdge(t *testing.T) {
+	input := `..........
+.........&
+..........
+.........`
+
+	found := checkForSymbol(0, 8, 2, strings.Split(input, "\n"))
+
 	assert.True(t, found)
 }
 
@@ -34,5 +89,6 @@ func TestCheckIfSymbol(t *testing.T) {
 	assert.False(t, checkIfSymbol('1'))
 	assert.False(t, checkIfSymbol('9'))
 	assert.True(t, checkIfSymbol('!'))
+	assert.True(t, checkIfSymbol('+'))
 	assert.True(t, checkIfSymbol(')'))
 }
