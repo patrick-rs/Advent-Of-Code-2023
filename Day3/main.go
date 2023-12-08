ede65c7938916e759b56a5b577d8284fcf75dbf5
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2023-12-07 20:05:38 -0500

    Refactor inputs everywhere!

--- Day3/main.go
+++ Day3/main.go
@@ -9,18 +9,23 @@ func main() {
 
 }
 
-func checkIfSymbol(i rune) bool {
+func checkIfSymbol(i byte) bool {
 	fmt.Println(i)
-	if unicode.IsDigit(i) {
+	if unicode.IsDigit(rune(i)) {
 		return false
 	}
-	if unicode.IsLetter(i) {
+	if unicode.IsLetter(rune(i)) {
 		return false
 	}
 	return true
 }
 
-func checkForSymbol(x1, y1, length int64, rows [][]byte) bool {
+func checkForSymbol(x1, y1, length int64, rows []string) bool {
 
+	for x := int64(0); x < length; x++ {
+		if checkIfSymbol(rows[y1][x]) {
+			return true
+		}
+	}
 	return false
 }
--- Day3/main_test.go
+++ Day3/main_test.go
@@ -13,13 +13,7 @@ func TestCheckForSymbol_ReturnsTrue(t *testing.T) {
 .....+....
 .........`
 
-	inputArray := [][]byte{}
-	for _, l := range strings.Split(input, "\n") {
-		inputArray = append(inputArray, []byte(l))
-	}
-
-	found := checkForSymbol(1, 4, 3, inputArray)
-
+	found := checkForSymbol(1, 4, 3, strings.Split(input, "\n"))
 	assert.True(t, found)
 }
 
@@ -29,27 +23,16 @@ func TestCheckForSymbol_ReturnsFalse(t *testing.T) {
 ..........
 .........`
 
-	inputArray := [][]byte{}
-	for _, l := range strings.Split(input, "\n") {
-		inputArray = append(inputArray, []byte(l))
-	}
-
-	found := checkForSymbol(1, 4, 2, inputArray)
+	found := checkForSymbol(1, 4, 2, strings.Split(input, "\n"))
 
 	assert.True(t, found)
 }
 
 func TestCheckIfSymbol(t *testing.T) {
-	input := 'a'
-	assert.False(t, checkIfSymbol(input))
-	input = 'z'
-	assert.False(t, checkIfSymbol(input))
-	input = '1'
-	assert.False(t, checkIfSymbol(input))
-	input = '9'
-	assert.False(t, checkIfSymbol(input))
-	input = '!'
-	assert.True(t, checkIfSymbol(input))
-	input = ')'
-	assert.True(t, checkIfSymbol(input))
+	assert.False(t, checkIfSymbol('a'))
+	assert.False(t, checkIfSymbol('z'))
+	assert.False(t, checkIfSymbol('1'))
+	assert.False(t, checkIfSymbol('9'))
+	assert.True(t, checkIfSymbol('!'))
+	assert.True(t, checkIfSymbol(')'))
 }
